bfc8cb20456b9cef8b83272e0222435077eb2342
arttttt <artem-bambalov@yandex.ru>
2018-10-15 21:19:08 +0300

    add root file impl
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

---
+++ app/src/main/java/com/arttttt/smokekerneltweaks/utils/su/RootFile.kt
@@ -0,0 +1,37 @@
+package com.arttttt.smokekerneltweaks.utils.su
+
+import com.arttttt.smokekerneltweaks.base.SingletonHolder
+
+class RootFile private constructor(){
+    companion object : SingletonHolder<RootFile>(::RootFile)
+
+    private val mSu by lazy { SU.getInstance() }
+
+    fun read(file: String): String? {
+        return mSu.runCommand("cat $file")
+    }
+
+    fun write(file: String, data: String, append: Boolean) {
+        val action = if (append)
+            ">>"
+        else
+            ">"
+
+        data.split("\\r?\\n").forEach {line ->
+            mSu.runCommand("echo $line $action $file")
+        }
+    }
+
+    fun remove(file: String) {
+        val command = if (isDirectory(file))
+            "\"rm -r $file\""
+        else
+            "rm $file"
+
+        mSu.runCommand(command)
+    }
+
+    fun isDirectory(file: String): Boolean {
+        return mSu.runCommand("[ -d '$file' ] && echo true") == "true"
+    }
+}
